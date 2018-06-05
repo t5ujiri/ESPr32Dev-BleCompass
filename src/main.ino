f1ae905a0850c56f870c575364bf7963663e9a1a
Takumi Ito <takumi10194617@gmail.com>
2018-06-05 18:18:40 +0900

    [WIP]

--- src/main.ino
+++ src/main.ino
@@ -66,19 +66,19 @@ LSM9DS1 imu;
 // Example I2C Setup //
 ///////////////////////
 // SDO_XM and SDO_G are both pulled high, so our addresses are:
-#define LSM9DS1_M	0x1E // Would be 0x1C if SDO_M is LOW
-#define LSM9DS1_AG	0x6B // Would be 0x6A if SDO_AG is LOW
+#define LSM9DS1_M 0x1E  // Would be 0x1C if SDO_M is LOW
+#define LSM9DS1_AG 0x6B // Would be 0x6A if SDO_AG is LOW
 
 ////////////////////////////
 // Sketch Output Settings //
 ////////////////////////////
 #define PRINT_CALCULATED
 //#define PRINT_RAW
-#define PRINT_SPEED 250 // 250 ms between prints
+#define PRINT_SPEED 250             // 250 ms between prints
 static unsigned long lastPrint = 0; // Keep track of print time
 
-// Earth's magnetic field varies by location. Add or subtract 
-// a declination to get a more accurate heading. Calculate 
+// Earth's magnetic field varies by location. Add or subtract
+// a declination to get a more accurate heading. Calculate
 // your's here:
 // http://www.ngdc.noaa.gov/geomag-web/#declination
 #define DECLINATION -8.58 // Declination (degrees) in Boulder, CO.
@@ -86,19 +86,36 @@ static unsigned long lastPrint = 0; // Keep track of print time
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <BLE2902.h>
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
-#define SERVICE_UUID        "ffeb4a0e-626a-4280-a57e-ec95d0fcf271"
+#define SERVICE_UUID "ffeb4a0e-626a-4280-a57e-ec95d0fcf271"
 #define CHARACTERISTIC_UUID "81084585-b0f4-4019-93b9-0f966eab1fd7"
 
-BLECharacteristic * pCharacteristic;
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharacteristic;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
 
-void setup() 
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
+    deviceConnected = true;
+  }
+
+  void onDisconnected(BLEServer *pServer)
+  {
+    deviceConnected = false;
+  }
+};
+
+void setup()
 {
   Serial.begin(115200);
-  
+  delay(100);
   // Before initializing the IMU, there are a few settings
   // we may need to adjust. Use the settings struct to set
   // the device's communication mode and addresses:
@@ -112,192 +129,106 @@ void setup()
   {
     Serial.println("Failed to communicate with LSM9DS1.");
     Serial.println("Double-check wiring.");
-    Serial.println("Default settings in this sketch will " \
-                  "work for an out of the box LSM9DS1 " \
-                  "Breakout, but may need to be modified " \
-                  "if the board jumpers are.");
+    Serial.println("Default settings in this sketch will "
+                   "work for an out of the box LSM9DS1 "
+                   "Breakout, but may need to be modified "
+                   "if the board jumpers are.");
     while (1)
       ;
   }
+  else
+  {
+    imu.calibrate();
+  }
 
   BLEDevice::init("BLECompass");
-  BLEServer *pServer = BLEDevice::createServer();
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
   BLEService *pService = pServer->createService(SERVICE_UUID);
   pCharacteristic = pService->createCharacteristic(
-    CHARACTERISTIC_UUID,
-    BLECharacteristic::PROPERTY_READ | 
-    BLECharacteristic::PROPERTY_NOTIFY
-  );
-  pCharacteristic->setWriteProperty(true);
-  pCharacteristic->setValue("Hello World!");
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_INDICATE);
+  pCharacteristic->addDescriptor(new BLE2902());
   pService->start();
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->start();
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting a client connection to notify...");
 }
 
 String buf;
-char c_buf[50];
+char c_buf[100];
 
 void loop()
 {
-  // Update the sensor values whenever new data is available
-  if ( imu.gyroAvailable() )
+  if (deviceConnected)
   {
-    // To read from the gyroscope,  first call the
-    // readGyro() function. When it exits, it'll update the
-    // gx, gy, and gz variables with the most current data.
-    imu.readGyro();
+    // Update the sensor values whenever new data is available
+    if (imu.gyroAvailable())
+    {
+      // To read from the gyroscope,  first call the
+      // readGyro() function. When it exits, it'll update the
+      // gx, gy, and gz variables with the most current data.
+      imu.readGyro();
+    }
+    if (imu.accelAvailable())
+    {
+      // To read from the accelerometer, first call the
+      // readAccel() function. When it exits, it'll update the
+      // ax, ay, and az variables with the most current data.
+      imu.readAccel();
+    }
+    if (imu.magAvailable())
+    {
+      // To read from the magnetometer, first call the
+      // readMag() function. When it exits, it'll update the
+      // mx, my, and mz variables with the most current data.
+      imu.readMag();
+    }
+
+    if ((lastPrint + PRINT_SPEED) < millis())
+    {
+      // printGyro();  // Print "G: gx, gy, gz"
+      // printAccel(); // Print "A: ax, ay, az"
+      // printMag();   // Print "M: mx, my, mz"
+      // // Print the heading and orientation for fun!
+      // // Call print attitude. The LSM9DS1's mag x and y
+      // // axes are opposite to the accelerometer, so my, mx are
+      // // substituted for each other.
+      // printAttitude(imu.ax, imu.ay, imu.az,
+      //              -imu.my, -imu.mx, imu.mz);
+      // Serial.println();
+      buf = "v:";
+      buf += String(imu.ax);
+      buf += ",";
+      buf += String(imu.ay);
+      buf += ",";
+      buf += String(imu.az);
+      buf += ",";
+      buf += String(imu.mx);
+      buf += ",";
+      buf += String(imu.my);
+      buf += ",";
+      buf += String(imu.mz);
+      buf.toCharArray(c_buf, buf.length());
+
+      pCharacteristic->setValue(c_buf);
+      pCharacteristic->notify();
+      lastPrint = millis(); // Update lastPrint time
+    }
   }
-  if ( imu.accelAvailable() )
+
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected)
   {
-    // To read from the accelerometer, first call the
-    // readAccel() function. When it exits, it'll update the
-    // ax, ay, and az variables with the most current data.
-    imu.readAccel();
+    delay(500);                  // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("start advertising");
+    oldDeviceConnected = deviceConnected;
   }
-  if ( imu.magAvailable() )
+  // connecting
+  if (deviceConnected && !oldDeviceConnected)
   {
-    // To read from the magnetometer, first call the
-    // readMag() function. When it exits, it'll update the
-    // mx, my, and mz variables with the most current data.
-    imu.readMag();
+    // do stuff here on connecting
+    oldDeviceConnected = deviceConnected;
   }
-  
-  if ((lastPrint + PRINT_SPEED) < millis())
-  {
-    // printGyro();  // Print "G: gx, gy, gz"
-    // printAccel(); // Print "A: ax, ay, az"
-    // printMag();   // Print "M: mx, my, mz"
-    // // Print the heading and orientation for fun!
-    // // Call print attitude. The LSM9DS1's mag x and y
-    // // axes are opposite to the accelerometer, so my, mx are
-    // // substituted for each other.
-    // printAttitude(imu.ax, imu.ay, imu.az, 
-    //              -imu.my, -imu.mx, imu.mz);
-    // Serial.println();
-    buf = "v:";
-    buf += String(imu.ax);
-    buf += ",";
-    buf += String(imu.ay);
-    buf += ",";
-    buf += String(imu.az);
-    buf += ",";
-    buf += String(imu.mx);
-    buf += ",";
-    buf += String(imu.my);
-    buf += ",";
-    buf += String(imu.mz);
-    buf.toCharArray(c_buf, buf.length());
-    
-    pCharacteristic->setValue(c_buf);
-    pCharacteristic->notify();
-    lastPrint = millis(); // Update lastPrint time
-  }
-}
-
-void printGyro()
-{
-  // Now we can use the gx, gy, and gz variables as we please.
-  // Either print them as raw ADC values, or calculated in DPS.
-  Serial.print("G: ");
-#ifdef PRINT_CALCULATED
-  // If you want to print calculated values, you can use the
-  // calcGyro helper function to convert a raw ADC value to
-  // DPS. Give the function the value that you want to convert.
-  Serial.print(imu.calcGyro(imu.gx), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcGyro(imu.gy), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcGyro(imu.gz), 2);
-  Serial.println(" deg/s");
-#elif defined PRINT_RAW
-  Serial.print(imu.gx);
-  Serial.print(", ");
-  Serial.print(imu.gy);
-  Serial.print(", ");
-  Serial.println(imu.gz);
-#endif
-}
-
-void printAccel()
-{  
-  // Now we can use the ax, ay, and az variables as we please.
-  // Either print them as raw ADC values, or calculated in g's.
-  Serial.print("A: ");
-#ifdef PRINT_CALCULATED
-  // If you want to print calculated values, you can use the
-  // calcAccel helper function to convert a raw ADC value to
-  // g's. Give the function the value that you want to convert.
-  Serial.print(imu.calcAccel(imu.ax), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcAccel(imu.ay), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcAccel(imu.az), 2);
-  Serial.println(" g");
-#elif defined PRINT_RAW 
-  Serial.print(imu.ax);
-  Serial.print(", ");
-  Serial.print(imu.ay);
-  Serial.print(", ");
-  Serial.println(imu.az);
-#endif
-
-}
-
-void printMag()
-{  
-  // Now we can use the mx, my, and mz variables as we please.
-  // Either print them as raw ADC values, or calculated in Gauss.
-  Serial.print("M: ");
-#ifdef PRINT_CALCULATED
-  // If you want to print calculated values, you can use the
-  // calcMag helper function to convert a raw ADC value to
-  // Gauss. Give the function the value that you want to convert.
-  Serial.print(imu.calcMag(imu.mx), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcMag(imu.my), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcMag(imu.mz), 2);
-  Serial.println(" gauss");
-#elif defined PRINT_RAW
-  Serial.print(imu.mx);
-  Serial.print(", ");
-  Serial.print(imu.my);
-  Serial.print(", ");
-  Serial.println(imu.mz);
-#endif
-}
-
-// Calculate pitch, roll, and heading.
-// Pitch/roll calculations take from this app note:
-// http://cache.freescale.com/files/sensors/doc/app_note/AN3461.pdf?fpsp=1
-// Heading calculations taken from this app note:
-// http://www51.honeywell.com/aero/common/documents/myaerospacecatalog-documents/Defense_Brochures-documents/Magnetic__Literature_Application_notes-documents/AN203_Compass_Heading_Using_Magnetometers.pdf
-void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
-{
-  float roll = atan2(ay, az);
-  float pitch = atan2(-ax, sqrt(ay * ay + az * az));
-  
-  float heading;
-  if (my == 0)
-    heading = (mx < 0) ? PI : 0;
-  else
-    heading = atan2(mx, my);
-    
-  heading -= DECLINATION * PI / 180;
-  
-  if (heading > PI) heading -= (2 * PI);
-  else if (heading < -PI) heading += (2 * PI);
-  else if (heading < 0) heading += 2 * PI;
-  
-  // Convert everything from radians to degrees:
-  heading *= 180.0 / PI;
-  pitch *= 180.0 / PI;
-  roll  *= 180.0 / PI;
-  
-  Serial.print("Pitch, Roll: ");
-  Serial.print(pitch, 2);
-  Serial.print(", ");
-  Serial.println(roll, 2);
-  Serial.print("Heading: "); Serial.println(heading, 2);
 }
